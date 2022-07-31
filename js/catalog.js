dfad33da0aa61f455e9257e71c41c027f408e61e
Stanislav <9226303141@mail.ru>
2022-07-31 21:21:12 +0500

    finished all mistakes on second page

---
+++ js/catalog.js
@@ -0,0 +1,11 @@
+const burgerOpen = document.querySelector(".menu-burger");
+const burgerClose = document.querySelector(".menu-burger__close");
+const menu = document.querySelector(".menu-body");
+
+burgerOpen.addEventListener("click", () => {
+  menu.style.display = "block";
+});
+
+burgerClose.addEventListener("click", () => {
+  menu.style.display = "none";
+});
