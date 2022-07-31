1f5eeabc41fac6bd509263bae4ee953012a00b90
Stanislav <9226303141@mail.ru>
2022-07-31 13:19:50 +0500

    add menu burger

--- js/script.js
+++ js/script.js
@@ -166,3 +166,15 @@ acc.forEach((el) => {
     }
   });
 });
+
+const burgerOpen = document.querySelector(".menu-burger");
+const burgerClose = document.querySelector(".menu-burger__close");
+const menu = document.querySelector(".menu-body");
+
+burgerOpen.addEventListener("click", () => {
+  menu.style.display = "block";
+});
+
+burgerClose.addEventListener("click", () => {
+  menu.style.display = "none";
+});
