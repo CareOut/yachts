f1ff5f9d6332a2c84aa9f04dd48185d9d87683d3
Stanislav <9226303141@mail.ru>
2022-08-06 15:18:13 +0500

    add modals tj the first page

--- js/script.js
+++ js/script.js
@@ -178,3 +178,29 @@ burgerOpen.addEventListener("click", () => {
 burgerClose.addEventListener("click", () => {
   menu.style.display = "none";
 });
+
+const modalBron = document.querySelector(".modal-bron");
+const buttonBron = document.querySelectorAll(".button__bron");
+const bronClose = document.querySelector(".cross__img");
+
+buttonBron.forEach((item) => {
+  item.addEventListener("click", () => {
+    modalBron.style.display = "flex";
+  });
+});
+
+bronClose.addEventListener("click", () => {
+  modalBron.style.display = "none";
+});
+
+const modalZayavka = document.querySelector(".modal-zayavka");
+const buttonMessage = document.querySelector(".messengers__message");
+const zayavkaClose = document.querySelector(".zayavka");
+
+buttonMessage.addEventListener("click", () => {
+  modalZayavka.style.display = "flex";
+});
+
+zayavkaClose.addEventListener("click", () => {
+  modalZayavka.style.display = "none";
+});
